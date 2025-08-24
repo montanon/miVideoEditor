3b38b00140ddae1a87dd1ecfe846b7d7b843d07b
montanon <montagna.p.92@hotmail.com>
2025-08-23 20:52:48 -0400

    feat: add models to init

--- mivideoeditor/core/__init__.py
+++ mivideoeditor/core/__init__.py
@@ -1,3 +1,19 @@
-"""
-Core module - Fundamental data structures and models
-"""
+"""Core module - Fundamental data structures and models."""
+
+from mivideoeditor.core.models import (
+    BlurRegion,
+    BoundingBox,
+    DetectionResult,
+    SensitiveArea,
+    Timeline,
+    ValidationResult,
+)
+
+__all__ = [
+    "BlurRegion",
+    "BoundingBox",
+    "DetectionResult",
+    "SensitiveArea",
+    "Timeline",
+    "ValidationResult",
+]
